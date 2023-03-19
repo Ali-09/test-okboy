b66aa7a73db882cf5bed0a0bd6c1de358cea9293
Ali-09 <yuzet.ali@gmail.com>
2023-03-19 17:05:07 -0600

    init structure project

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// Atoms
+// Molecules
+//Organisms
+//Templates
+//Pages
+export { default as Home } from 'components/pages/Home';
---
+++ src/components/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Test okboy</h1>
+  )
+}
+
+export default Home
