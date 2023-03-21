56e0ab5246a5a53b3a3159e6997fdfe21b551f69
Ali-09 <yuzet.ali@gmail.com>
2023-03-20 22:18:57 -0600

    feat: add favorites and sort favorites coins

--- src/components/organisms/TableCoins/index.js
+++ src/components/organisms/TableCoins/index.js
@@ -1,42 +1,54 @@
-import "./styles.scss"
-import PropTypes from 'prop-types'
+import "./styles.scss";
+import PropTypes from "prop-types";
+import FavoriteIcon from "assets/icons/FavoriteIcon";
 
-const TableCoins = ({ coins }) => {
+const TableCoins = ({ coins, handleFavorites, favorites }) => {
   return (
     <div className="wrapper-table">
-        <table>
-            <thead>
-                <tr>
-                    <th align="center">#</th>
-                    <th align="left">Nombre</th>
-                    <th align="center">Precio</th>
-                    <th align="center">Precio BTC</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    coins.map(coin => (
-                        <tr key={coin.id}>
-                            <td align="center">{coin.rank}</td>
-                            <td align="left">
-                                <div className="table--coin-item">
-                                    <img src={coin.icon} alt={coin.name}/>
-                                    <p>{coin.name}</p>
-                                </div>
-                            </td>
-                            <td align="center">{coin.price.toFixed(2)} MX</td>
-                            <td align="center">{coin.priceBtc}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th align="center">#</th>
+            <th align="left">Nombre</th>
+            <th align="center">Precio</th>
+            <th align="center">Precio BTC</th>
+            <th align="center">Cambio 24H</th>
+            <th align="center">Favoritos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td align="center">{coin.rank}</td>
+              <td align="left">
+                <div className="table--coin-item">
+                  <img src={coin.icon} alt={coin.name} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td align="center">{coin.price.toFixed(2)} MX</td>
+              <td align="center">{coin.priceBtc}</td>
+              <td align="center">{coin.priceChange1d}</td>
+              <td align="center">
+                <button onClick={() => handleFavorites(coin.id)}>
+                  <FavoriteIcon
+                    fill={favorites.includes(coin.id) ? '#E90064' : ''}
+                    stroke={!favorites.includes(coin.id) ? '#E90064' : ''}
+                  />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 TableCoins.propTypes = {
-    coins: PropTypes.array.isRequired
-}
+  coins: PropTypes.array.isRequired,
+  handleFavorites: PropTypes.func.isRequired,
+  favorites: PropTypes.array.isRequired,
+};
 
-export default TableCoins
+export default TableCoins;
